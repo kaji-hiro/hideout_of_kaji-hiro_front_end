c87cf3ac5daccba8f7c0fa4cf4c0a72901ee215d
kaji-hiro <kajihiro190206@gmail.com>
2021-05-20 13:05:54 +0900

    fix pages

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="kaji-hiro's hideout"
         />
         <meta
           property="og:image"
